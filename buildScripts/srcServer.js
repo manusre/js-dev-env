c95d8c6fae0ee4b653f07842e754075ccee64386
Manu Sreenivasachar <manusre@microsoft.com>
2018-12-04 14:34:34 -0800

    first checkin

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+var port = 3000;
+var app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName": "Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy", "lastName": "Norton", "email":"tammy@gmail.com"},
+    {"id": 3, "firstName":"Tina", "lastName": "Lee", "email":"tina@gmail.com"}
+  ]);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if(err) {
+    console.log(err); // eslint-disable-line no-console
+  } else {
+    open('http://localhost:' + port);
+  }
+})
